06eb35edddb171c841e35debd7591e70373f9a6b
Roald Ruiter <ruiter@inspire.ethz.ch>
2022-07-03 15:02:57 +0200

    extract single group from match
    (cherry picked from commit a947183a6cf63a11f4b2772c1979731720c0dc3f)

--- Scraper/Program.fs
+++ Scraper/Program.fs
@@ -14,9 +14,10 @@ let win32pattern = @"<p>(\d+)<\/p>.*?<p>(0x[0-9A-F]{8})<\/p>.+?<p>([A-Z_<br \/>]
 let main argv =
     let htmlContent = File.ReadAllText("../../../win32_errors.html")
     Regex(win32pattern, RegexOptions.Singleline).Matches(htmlContent)
-    |> Seq.map (fun m -> m.Groups |> Seq.tail)
     |> Seq.take 3
-    |> Seq.iter (printfn "%A")
+    |> Seq.map (fun m -> m.Groups |> Seq.tail |> Seq.toArray)
+    |> Seq.map (fun x -> $"{x.[2]}")
+    |> Seq.iter (printfn "%s")
 
     0
     
